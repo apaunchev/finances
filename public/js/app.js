42c1804e4702b6fcb2354d4c706968669cf5fa91
Angel Paunchev <apaunchev@gmail.com>
2017-03-18 14:22:42 +0200

    refactor(app): move Firebase config

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,5 @@
 /* global angular, firebase */
 
-var config = {
-  apiKey: "AIzaSyDO5aGZX4nztCKYpqFY2PgE-XpJrJB9fwg",
-  authDomain: "finances-app-1191e.firebaseapp.com",
-  databaseURL: "https://finances-app-1191e.firebaseio.com",
-  storageBucket: "finances-app-1191e.appspot.com",
-  messagingSenderId: "1006685798394"
-};
-
-firebase.initializeApp(config);
-
 angular.module('finances', [
   'ngRoute',
   'ngStorage',
@@ -24,6 +14,14 @@ angular.module('finances', [
   .config(['$locationProvider', '$routeProvider', '$compileProvider', function ($locationProvider, $routeProvider, $compileProvider) {
     $routeProvider.otherwise({ redirectTo: '/' })
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/)
+    var config = {
+      apiKey: 'AIzaSyDO5aGZX4nztCKYpqFY2PgE-XpJrJB9fwg',
+      authDomain: 'finances-app-1191e.firebaseapp.com',
+      databaseURL: 'https://finances-app-1191e.firebaseio.com',
+      storageBucket: 'finances-app-1191e.appspot.com',
+      messagingSenderId: '1006685798394'
+    }
+    firebase.initializeApp(config)
   }])
 
   .directive('customOnChange', function () {
