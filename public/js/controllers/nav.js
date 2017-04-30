20c289284ef3c8dce6d09f20994318de36970c18
Angel Paunchev <apaunchev@gmail.com>
2017-04-30 11:56:35 +0300

    improve nav

--- public/js/controllers/nav.js
+++ public/js/controllers/nav.js
@@ -1,26 +1,35 @@
 /* global angular */
 
-function NavCtrl ($scope, $location, $window, authService) {
-  $scope.auth = authService
-  $scope.user = null
+(function () {
+  'use strict'
 
-  $scope.auth.onAuthStateChanged(firebaseUser => {
-    $scope.user = firebaseUser
-  })
+  angular
+    .module('finances.nav', ['ngRoute'])
+    .controller('NavCtrl', ['$scope', '$location', '$window', 'authService', NavCtrl])
 
-  $scope.signIn = function () {
-    $scope.auth.signIn()
-      .then(res => {
-        $location.path('/dashboard')
-      })
-      .catch(err => console.error(err))
+  function NavCtrl ($scope, $location, $window, authService) {
+    $scope.auth = authService
+    $scope.user = null
+
+    $scope.isActive = (path) => {
+      return $location.path().substr(0, path.length) === path
+    }
+
+    $scope.auth.onAuthStateChanged(firebaseUser => {
+      $scope.user = firebaseUser
+    })
+
+    $scope.signIn = () => {
+      $scope.auth.signIn()
+        .then(res => {
+          $location.path('/dashboard')
+        })
+        .catch(err => console.error(err))
+    }
+
+    $scope.signOut = () => {
+      $scope.auth.signOut()
+      $window.location.reload()
+    }
   }
-
-  $scope.signOut = function () {
-    $scope.auth.signOut()
-    $window.location.reload()
-  }
-}
-
-angular.module('finances.nav', ['ngRoute'])
-  .controller('NavCtrl', ['$scope', '$location', '$window', 'authService', NavCtrl])
+})()
