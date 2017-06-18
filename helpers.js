38b93aab3a5eaa60091da1004b04b892c8507131
Angel Paunchev <apaunchev@gmail.com>
2017-06-18 11:23:13 +0300

    add helper function

--- helpers.js
+++ helpers.js
@@ -1,2 +1,8 @@
 exports.moment = require('moment');
+
+exports.getMonthName = (month) => {
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return monthNames[parseFloat(month) - 1];
+};
+
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
