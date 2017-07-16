feec7f776b35ba823e8b1f4e9ca77b40811ba49a
Angel Paunchev <apaunchev@gmail.com>
2017-07-16 15:56:41 +0300

    replace gulp with webpack

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+import '../sass/style.scss';
+
+import { $, $$ } from './modules/bling';
+import feather from 'feather-icons';
+
+feather.replace({
+  'stroke-width': 1
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './public/js/app.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader!sass-loader'
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:1234/',
+      notify: false
+    })
+  ],
+  devtool: 'source-map'
+};
