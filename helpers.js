e9d997abf596125a1c06ef3787e0511ea4f38ff0
Angel Paunchev <apaunchev@gmail.com>
2017-08-03 16:27:53 +0300

    extract settings menu in a helper object

--- helpers.js
+++ helpers.js
@@ -5,4 +5,10 @@ exports.getMonthName = (month) => {
   return monthNames[parseFloat(month)];
 };
 
+exports.settingsMenu = [
+  { 'url': '/account', title: 'Account', icon: 'user' },
+  { 'url': '/categories', title: 'Categories', icon: 'folder' },
+  { 'url': '/budget', title: 'Budget', icon: 'bar-chart-2' }
+];
+
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
