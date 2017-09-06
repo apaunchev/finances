d54ae33fa0726adda7fc3a9c0b583f0946ffdf34
Angel Paunchev <apaunchev@gmail.com>
2017-09-06 13:52:05 +0300

    fix mongo deprecation warning

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, { useMongoClient: true });
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
   console.error(`🚫 → ${err.message}`);
